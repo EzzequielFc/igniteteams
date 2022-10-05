48c9af119a640633abf7515f052a62082a32de1d
Ezequiel <contato.ezequielf@gmail.com>
2022-10-05 08:19:04 -0300

    a

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,26 @@
-import { Header } from '@components/Header';
-import { Highlight } from '@components/Highlight';
+import { useState } from "react";
+import { FlatList } from "react-native";
 
-import { Container } from './styles';
+import { GroupCard } from "@components/GroupCard";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+
+import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocket", "Amigos"]);
+
   return (
     <Container>
       <Header />
 
-      <Highlight 
-        title='Turmas'
-        subtitle='jogue com sua turma'
+      <Highlight title="Turmas" subtitle="jogue com sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
       />
     </Container>
   );
 }
-
-

b34a920033a2012d34f274985d41aff54fe524e7
Ezequiel <contato.ezequielf@gmail.com>
2022-10-11 15:34:38 -0300

    oi

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import theme from "./src/theme"
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { StatusBar } from "react-native";
+import { NewGroup } from "@screens/NewGroup";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,7 +17,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-    { fontsLoaded ? <Groups /> : <Loading />}
+    { fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,8 @@ import { FlatList } from "react-native";
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpyt";
+import { Button } from "@components/Button";
 
 import { Container } from "./styles";
 
@@ -20,6 +22,12 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => <ListEmpty message="Quel tal cadastrar a primeira turma?"/>}
+      />
+
+      <Button 
+        title="Criar nova Turma"
       />
     </Container>
   );
